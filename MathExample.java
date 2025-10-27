60df6c812989b33d92dccd1e324b3e9c02cd850b
naitik2424 <naitik242424@gmail.com>
2025-10-27 19:32:42 +0530

    MathExample

---
+++ MathExample.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MathExample {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+
+        double result;
+        result = Math.pow(2, 3);
+        System.out.println(result);
+
+        double a;
+        double b;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter side a:");
+        
+        a = scanner.nextDouble();
+
+        System.out.println("Enter side b:");
+        b = scanner.nextDouble();
+
+        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println(c);
+        
+
+        scanner.close();
+        
+    }
+
+    
+}
+
--- RandomExample.java
+++ RandomExample.java
@@ -1,29 +1,25 @@
 import java.util.Random;
 
-
 public class RandomExample {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Random random = new Random();
 
         int number;
         double number1;
         boolean isHead;
- 
-        number = random.nextInt(1,7);
+
+        number = random.nextInt(1, 7);
         System.out.println("Random number between 1 to 6: " + number);
 
-        number1 = random.nextDouble(1,7);
-        System.out.println("Random number between 1.0 to 6.0: " + number1);
+        number1 = random.nextDouble(1, 7);
+        System.out.printf("Random number between 1.0 to 6.0: %.2f%n", number1);
 
         isHead = random.nextBoolean();
-        if(isHead){
+        if (isHead) {
             System.out.println("It's Heads");
-        }
-        else{
+        } else {
             System.out.println("It's Tails");
         }
-
     }
-    
 }
